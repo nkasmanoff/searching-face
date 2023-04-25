8bd7b648904389e288d5bec6dba68eb204e6b7a0
nkasmanoff <nkasmanoff@gmail.com>
2023-04-25 07:06:59 -0400

    update: py structure

--- vectorize_dataset.py
+++ vectorize_dataset.py
@@ -1,5 +1,10 @@
 from datasets import load_dataset
 from helpers import clean_up_tags
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.vectorstores import Chroma
+from langchain.document_loaders import DataFrameLoader
+
 
 def load_descriptions_data():
     hf_datasets = load_dataset('nkasmanoff/huggingface-datasets')
@@ -11,3 +16,16 @@ def load_descriptions_data():
     hf_df = hf_df[['id','description_full']]
 
     return hf_df
+
+
+def create_db(hf_df):
+    loader = DataFrameLoader(hf_df, page_content_column="description_full")
+    documents = loader.load()
+    # split the documents into chunks
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(documents)
+    # select which embeddings we want to use
+    embeddings = OpenAIEmbeddings()
+    # create the vectorestore to use as the index
+    db = Chroma.from_documents(texts, embeddings)    
+    return db
